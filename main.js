60f7c93accaf8736d64f32b32346b46fd6fc5a43
chaturbhuj02 <chaturbhuj02@gmail.com>
2024-03-05 23:04:38 +1100

    added button flashing

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let gameseq=[];
 let userseq=[];
+
+let btns = ['one', 'two', 'three', 'four'];
+
 let started=false;
 let level=0;
 
@@ -18,6 +21,37 @@ document.addEventListener("keydown", ()=>{
 function levelup(){
     level++;
     heading2.innerText= `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*4);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    gameFlash(randBtn);
      
 }
 
+function gameFlash(btn){
+    btn.classList.add('flash');
+    setTimeout(()=>{
+        btn.classList.remove('flash');
+    }, 250);
+};
+
+function userFlash(btn){
+    btn.classList.add('userflash');
+    setTimeout(()=>{
+        btn.classList.remove('userflash');
+    }, 250);
+};
+
+function btnPress(){
+    let btn = this;
+    userFlash(btn);
+};
+
+let allBtns = document.querySelectorAll('.box');
+for (allBtn of allBtns){
+    allBtn.addEventListener('click', btnPress);
+}
